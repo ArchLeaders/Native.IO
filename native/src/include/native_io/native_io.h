c4b50cbc642f353f7410c1afbe07c251b6fcf232
ArchLeaders <archparliament@gmail.com>
2023-05-11 09:37:31 -0700

    Include extern "C" in macro

--- native/src/include/native_io/native_io.h
+++ native/src/include/native_io/native_io.h
@@ -4,15 +4,13 @@
 #include <vector>
 
 #if _WIN32
-#define CEAD __declspec(dllexport)
+#define CEAD extern "C" __declspec(dllexport)
 #else
-#define CEAD
+#define CEAD extern "C"
 #endif
 
-extern "C" {
-CEAD void GetVectorHandle(std::vector<std::uint8_t>* vector, std::uint8_t** dst, std::uint32_t* dst_len);
+CEAD void GetVectorHandle(std::vector<std::uint8_t>* vector, std::uint8_t** dst, std::size_t* dst_len);
 CEAD bool FreeVectorHandle(std::vector<std::uint8_t>* vector);
 
-CEAD void GetStringHandle(std::string* str, const char** dst, std::uint32_t* dst_len);
+CEAD void GetStringHandle(std::string* str, const char** dst, std::size_t* dst_len);
 CEAD bool FreeStringHandle(std::string* str);
-}
