9c87c4fe3b6e01e2ff17a8375f2556bae52d7411
ArchLeaders <archparliament@gmail.com>
2023-05-11 01:08:23 -0700

    Update test project

---
+++ tests/DummyLib.cs
@@ -0,0 +1,8 @@
+﻿using Native.IO.Services;
+
+namespace Native.IO.Tests;
+
+public class DummyLib : NativeLibrary<DummyLib>, INativeLibrary
+{
+    protected override string Name { get; } = "Dummy";
+}
---
+++ tests/Program.cs
@@ -0,0 +1,7 @@
+﻿using Native.IO.Services;
+using Native.IO.Tests;
+
+NativeLibraryManager.RegisterAssembly(typeof(Program).Assembly)
+    .Register(DummyLib.Shared, out bool dummyLibLoaded);
+
+Console.WriteLine($"Dummy Lib Loaded: {dummyLibLoaded}");
