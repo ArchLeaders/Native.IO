7c20c2544dff89c237f377725f9c93f521325636
ArchLeaders <archparliament@gmail.com>
2023-05-11 01:55:24 -0700

    Create native common library

---
+++ native/src/native_io.cpp
@@ -0,0 +1,21 @@
+#include "native_io/native_io.h"
+
+void GetVectorHandle(std::vector<std::uint8_t>* vector, std::uint8_t** dst, std::size_t* dst_len) {
+  *dst = vector->data();
+  *dst_len = vector->size();
+}
+
+bool FreeVectorHandle(std::vector<std::uint8_t>* vector) {
+  delete vector;
+  return true;
+}
+
+void GetStringHandle(std::string* str, const char** dst, std::size_t* dst_len) {
+  *dst = str->c_str();
+  *dst_len = str->length();
+}
+
+bool FreeStringHandle(std::string* str) {
+  delete str;
+  return true;
+}
