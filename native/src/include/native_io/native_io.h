38cb4c7d686a6a482e99364f9c33eaadda2838c6
ArchLeaders <archparliament@gmail.com>
2023-05-11 09:34:41 -0700

    extern "C" on exported functions

--- native/src/include/native_io/native_io.h
+++ native/src/include/native_io/native_io.h
@@ -9,8 +9,10 @@
 #define CEAD
 #endif
 
+extern "C" {
 CEAD void GetVectorHandle(std::vector<std::uint8_t>* vector, std::uint8_t** dst, std::uint32_t* dst_len);
 CEAD bool FreeVectorHandle(std::vector<std::uint8_t>* vector);
 
 CEAD void GetStringHandle(std::string* str, const char** dst, std::uint32_t* dst_len);
 CEAD bool FreeStringHandle(std::string* str);
+}
